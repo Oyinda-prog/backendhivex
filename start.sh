6f6d0b63d4940c053df1bb73aee2ebdc450b6be5
Oyinda-prog <115878426+Oyinda-prog@users.noreply.github.com>
2026-07-17 11:35:13 +0200

    Rename Todos folder to lowercase

--- start.sh
+++ start.sh
@@ -3,7 +3,11 @@
 mkdir -p storage/framework/sessions
 mkdir -p storage/framework/cache/data
 mkdir -p storage/framework/views
+mkdir -p storage/logs
 
+touch storage/logs/laravel.log
+
+chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
 php artisan optimize:clear
