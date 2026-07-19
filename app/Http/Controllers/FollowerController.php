fe62f3f0592d324fbd69830b69d1f3fb079efc7b
Oyinda-prog <115878426+Oyinda-prog@users.noreply.github.com>
2026-07-19 14:50:50 +0200

    WIP

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -36,35 +36,24 @@ class FollowerController extends Controller
     }
 }
 
-    public function allfollowers(Request $req){
-      $follower=Followers::where('following_id',$req->student_id)->where('status','follow')->get();
-      if($follower->count()>0){
-return json_encode([
-               'status'=>200,
-               'follower'=>$follower
-           ]);
-      }
-      else{
-        return json_encode([
-               'status'=>201,
-               'msg'=>0
-           ]);
-      }
-    }
+    public function allfollowers(Request $req)
+{
+    $followers = Followers::where('following_id', $req->student_id)->get();
 
-    public function allfollowing(Request $req){
-      $following=Followers::where('follower_id',$req->student_id)->where('status','follow')->get();
-      if($following->count()>0){
-return json_encode([
-               'status'=>200,
-               'following'=>$following
-           ]);
-      }
-      else{
-        return json_encode([
-               'status'=>201,
-               'msg'=>0
-           ]);
-      }
-    }
+    return response()->json([
+        'status' => true,
+        'followers' => $followers
+    ]);
+}
+
+    public function allfollowing(Request $req)
+{
+    $following = Followers::where('follower_id', $req->student_id)->get();
+
+    return response()->json([
+        'status' => true,
+        'following' => $following
+    ]);
+}
+
 }
