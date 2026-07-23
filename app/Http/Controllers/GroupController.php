c6a4e9c8759bd6a54ad5a40c6fc6b6354874b3ae
Oyinda-prog <115878426+Oyinda-prog@users.noreply.github.com>
2026-07-23 13:34:08 +0200

    WIP8

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,83 +7,100 @@ use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
- public function creategroup(Request $request){
-   try {
-    $group=new Group();
-    $group->student_id = $request->student_id;
-    $group->uniquenumber = $request->uniquenumber;
-    $group->name = $request->groupname;
-    $group->privacy = $request->privacy;
-   $save = $group->save();
-    if ($save) {
-        return response()->json([
-            'status' => true,
-            'message' => 'Group created successfully',
-            'group' => $group,
-        ], 200);
-    }
-
-     return response()->json([
-            'status' => false,
-            'message' => 'Unable to create group',
-        ], 400);
-
-
-    } catch (\Exception $e) {
-       return response()->json(
-        [
-            'message' => $e->getMessage(),
-            'file' => $e->getFile(),
-            'line' => $e->getLine(),
-        ], 500);
-    }
-
-}
- public function getgroup(Request $req){
-   $group=Group::where('student_id',$req->student_id)->where('uniquenumber',$req->uniquenumber)->first();
-   if($group){
-return response()->json([
-            'message' => 'Group found',
-            'status' => 200,
-            'group'=>$group
-        ]);
-   }
-   else{
-     response()->json(
-        ['status'=>500,
-        'message'=>'Group does not exist']
-     );
-   }
- }
-
-  public function getallgroups(int $id){
+  public function creategroup(Request $request){
     try {
-        $allgroups = Group::where('student_id', $id)->get();
-        if ($allgroups->isEmpty()){
+        $group=new Group();
+        $group->student_id = $request->student_id;
+        $group->uniquenumber = $request->uniquenumber;
+        $group->name = $request->groupname;
+        $group->privacy = $request->privacy;
+        $save = $group->save();
+        if ($save) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Group created successfully',
+                'group' => $group,
+            ], 200);
+        }
+
+        return response()->json([
+                'status' => false,
+                'message' => 'Unable to create group',
+            ], 400);
+
+
+        } catch (\Exception $e) {
+        return response()->json(
+            [
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ], 500);
+        }
+    }
+
+    public function getgroup(string $uniquenumber){
+        $group=Group::where('uniquenumber', $uniquenumber)->first();
+        try
+        {
+            if ($group){
+                return response()->json
+                (
+                    [
+                        'status' => true,
+                        'message' => 'Group found',
+                        'group'=>$group
+                    ], 200
+                );
+            }
+
+            return  response()->json(
+                [
+                    'status'=> false,
+                    'message'=>'Group does not exist'
+                ], 200
+            );
+
+        } catch (\Exception $e) {
             return response()->json(
-               [
-                  'status'=> false,
-                  'message'=> 'No groups created yet!'
-               ], 200
+                [
+                    'message' => $e->getMessage(),
+                    'file' => $e->getFile(),
+                    'line' => $e->getLine()
+                ]
             );
         }
 
-        return response()->json(
-            [
-                'status'=> true,
-                'message'=> 'Group found!',
-                'allgroups' => $allgroups
-            ], 200
-        );
-    } catch (\Exception $e) {
-        return response()->json(
-            [
-                'file'=>$e->getFile(),
-                'line'=>$e->getLine(),
-                'message'=>$e->getMessage(),
-            ], 500
-        );
     }
 
-  }
+  public function getallgroups(int $id){
+        try {
+            $allgroups = Group::where('student_id', $id)->get();
+            if ($allgroups->isEmpty()){
+                return response()->json(
+                [
+                    'status'=> false,
+                    'message'=> 'No groups created yet!'
+                ], 200
+                );
+            }
+
+            return response()->json(
+                [
+                    'status'=> true,
+                    'message'=> 'Group found!',
+                    'allgroups' => $allgroups
+                ], 200
+            );
+        } catch (\Exception $e) {
+            return response()->json
+            (
+                [
+                    'file'=>$e->getFile(),
+                    'line'=>$e->getLine(),
+                    'message'=>$e->getMessage(),
+                ], 500
+            );
+        }
+    }
 }
